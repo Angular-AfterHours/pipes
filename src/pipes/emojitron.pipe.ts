a656b4c928b8b1d9c6448ba1362680d35d129121
Shmuela <shmuela@500tech.com>
2016-12-16 22:58:00 +0200

    refactor emojitron pipe

--- src/pipes/emojitron.pipe.ts
+++ src/pipes/emojitron.pipe.ts
@@ -1,45 +1,38 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+const map = {
+    'lv':'😍',
+    'sm':'😃',
+    'lf':'😂',
+    'lk':'👍',
+    'po':'💩',
+    'mk':'🙈',
+    'cy':'😥',
+    'md':'😡',
+    'fr':'🔥',
+    'zz':'😴',
+    'mn':'🌜',
+    'sn':'🌞',
+    'cl':'😎',
+    'sp':'😱',
+    'wk':'😜',
+    'sd':'☹️',
+    'al':'👽',
+    'pr':'🙏',
+    'sk':'😷',
+    'ks':'💋',
+    'ct':'🐱',
+    'kl':'🐨',
+    'pn':'🐼',
+    'ap':'🍏',
+    'cf':'☕️',
+    'nd':'🤓'
+};
+
+
 @Pipe({name: 'emojitron'})
 export class EmojitronPipe implements PipeTransform {
-    transform(value:string, args:string[]):string { 
-        var map = {
-            'lv':'😍',
-            'sm':'😃',
-            'lf':'😂',
-            'lk':'👍',
-            'po':'💩',
-            'mk':'🙈',
-            'cy':'😥',
-            'md':'😡',
-            'fr':'🔥',
-            'zz':'😴',
-            'mn':'🌜',
-            'sn':'🌞',
-            'cl':'😎',
-            'sp':'😱',
-            'wk':'😜',
-            'sd':'☹️',
-            'al':'👽',
-            'pr':'🙏',
-            'sk':'😷',
-            'ks':'💋',
-            'ct':'🐱',
-            'kl':'🐨',
-            'pn':'🐼',
-            'ap':'🍏',
-            'cf':'☕️',
-            'nd':'🤓'
-        }
-
-        while(true) {
-            var index = value.indexOf("*");
-            if(index == -1) {
-                break;
-            }
-            var sub = value.substr(index, 3);
-            value = value.replace(sub, map[sub.substr(1,2)]);
-        }
-        return value;
+    transform(value:string, args:string[]):string {
+        return value.replace(/\*([a-z]{2})/g, (match) => map[match.substr(1, 2)] || match);
     }
 }
