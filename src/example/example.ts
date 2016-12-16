644e8ba08c516faee13b11c36977638e4b2d9257
Shmuela <shmuela@500tech.com>
2016-12-16 22:57:29 +0200

    add example: emojitron

--- src/example/example.ts
+++ src/example/example.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { EmojitronPipe } from '../pipes/emojitron.pipe';
 
 @Component({
   selector: 'examples',
-  pipes: [],
+  pipes: [EmojitronPipe],
   template:`
-   <h1>Pipes Examples</h1>
-   ` ,
+    <h1>Pipes Examples</h1>
+    <ul>
+      <li>emojitron: {{ emojiExample | emojitron }}</li>
+    </ul>
+  ` ,
 })
 
-export class Examples {}
+export class Examples {
+  emojiExample = 'This is *lk an emoji *sm example *cf. Example for *no match.';
+}
